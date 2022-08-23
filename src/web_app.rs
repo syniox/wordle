97992122c89e95b0102a0a7a35be7661fab7d87b
syniox <596919077@qq.com>
2022-08-23 20:22:33 +0800

    feat: implement focus

--- src/web_app.rs
+++ src/web_app.rs
@@ -1,15 +1,23 @@
 use yew::{
     events::{ InputEvent, KeyboardEvent, MouseEvent },
     function_component, classes, 
-    Properties, html, Callback, Component, Context, Html
+    Properties, html, Callback, Component, Context, Html,
+    NodeRef
 };
 //use wasm_bindgen::JsCast;
 //use wasm_bindgen::closure::Closure;
 //use wasm_bindgen::UnwrapThrowExt;
 extern crate web_sys;
-use yew::NodeRef;
+use web_sys::HtmlInputElement;
 
 mod utils;
+mod game;
+use game::{Game,Stats};
+
+/*
+mod args;
+use args::Args;
+*/
 
 enum Msg {
     Input(InputEvent, usize, usize), // row, col
@@ -18,6 +26,7 @@ enum Msg {
 }
 
 struct App{
+    game: Game,
     board: Vec<Vec<NodeRef>>,
     focus: (usize, usize)
 }
@@ -45,6 +54,65 @@ pub fn keybr_button(props: &KeybrButtonProps) -> Html {
     }
 }
 
+impl App {
+    // focus on the right thing
+    fn get_focus_ref(&self) -> NodeRef {
+        self.board[self.focus.0][self.focus.1].clone()
+    }
+    fn apply_focus(&self){
+        if let Some(input) = self.get_focus_ref().cast::<HtmlInputElement>() {
+            input.focus().expect(&format!("focus_next error: {:?}", self.focus));
+        }
+    }
+    fn focus_next(&mut self, enter: bool) {
+        if self.focus.1 < utils::ROUNDS - 1 {
+            self.focus.1 += 1;
+        } else if enter == true {
+            self.focus.1 = 0;
+            self.focus.0 += 1;
+            if self.focus.0 == utils::ROUNDS {
+                return;
+            }
+        }
+        self.apply_focus();
+    }
+    fn focus_prev(&mut self) {
+        if self.focus.1 > 0 {
+            self.focus.1 -= 1;
+            self.apply_focus();
+        }
+    }
+
+    pub fn backspace(&mut self){
+        let node = self.board[self.focus.0][self.focus.1].get().unwrap();
+        log::info!("backspace on {:?}", self.focus);
+        log::info!("node_value:{}", node.node_value().unwrap_or(String::from("None")));
+        if node.node_value().is_none(){
+            self.focus_prev();
+        }
+        if self.focus != (0, 0) {
+            assert!(!node.node_value().unwrap().is_empty());
+        }
+        node.set_node_value(Some(""));
+    }
+    pub fn linebreak(&mut self){
+        if self.focus.1 != utils::ROUNDS - 1 {
+            return;
+        }
+        let guess = self.board[self.focus.0].iter()
+            .map(|x| {
+                let node = x.get().unwrap();
+                assert!(node.node_value().unwrap().len() == 1);
+                node.node_value().unwrap().pop().unwrap()
+            })
+            .collect::<String>();
+        // TODO: connect with game
+    }
+    pub fn insert(&mut self, c: char) {
+        //let node = self.get_focus_ref().get().unwrap();
+    }
+}
+
 // Keyboard viewing function
 fn keyarr2html<T: yew::Component>(arr: &'static [char], ctx: &Context<T>) -> Html
 where <T as yew::Component>::Message: From <Msg> {
@@ -69,43 +137,39 @@ impl Component for App {
 
     fn create(_ctx: &Context<Self>) -> Self {
         Self {
+            game: Game::new(),
             board: vec![vec![NodeRef::default(); utils::LEN]; utils::ROUNDS],
             focus: (0, 0)
         }
     }
-
-    /*fn rendered(&mut self, ctx: &Context<Self>, first_renderer: bool){
-        if !first_renderer { return; }
-        let document = gloo::utils::document();
-        let listener = EventListener::new(&document, "keydown", |event| {
-            let event = event.dyn_ref::<web_sys::KeyboardEvent>().unwrap_throw();
-            // TODO: try to reuse the key
-            // ctx.link().callback(|_: KeyboardEvent| Msg::Press(event.clone()));
-        });
-        self.kbd_listener.replace(listener);
-    }*/
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg{
             Msg::Input(input, r, c) => {
                 log::info!("typed on row {r} col {c}");
                 match input.input_type().as_str() {
-                    "insertText" => log::info!("insert: {}",input.data().unwrap()),
-                    "deleteContentBackward" => log::info!("Backspace"), // please don't do anything here
-                    e => panic!("Unknown type: {}",e)
+                    "insertText" => {
+                        let mut s = input.data().unwrap();
+                        log::info!("insert: {}",s);
+                        assert!(s.len() == 1);
+                        self.insert(s.pop().unwrap());
+                    }
+                    _ => unreachable!()
                 }
             }
             Msg::Press(event, r, c) => {
                 log::info!("Pressed: {}", event.key());
                 if event.key() == "Enter" {
-
+                    self.linebreak();
                 }
                 if event.key() == "Backspace" {
-
+                    self.backspace();
                 }
             }
             Msg::Click(c) => {
+                // TODO: backspace and enter
                 log::info!("Clicked: {}", c);
+                self.insert(c);
             }
         }
         true
@@ -139,6 +203,7 @@ impl Component for App {
                     <div class={"row"}> {
                         x.iter().enumerate().map(|(col, _)| html! {
                             <input class={"tile"}
+                            ref={self.board[row][col].clone()}
                             maxlength={1}
                             onkeydown = {onkeydown(row,col)}
                             oninput = {oninput(row, col)}
